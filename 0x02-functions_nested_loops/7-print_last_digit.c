01b18ccb7ea04f3823a2517afef8e0def0e2562e
petosagie <petosagie@gmail.com>
2022-06-07 15:08:33 -0700

    Task 2 and Task 3

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/** * _abs - Computes the absolute value * of an integer. * * @i: input number as an integer. * * Return: absolute 
+ value */
+int _abs(int i) { if (i >= 0) { return (i);
+	}
+	else { return (i * -1);
+	}
+}
+chmod u+x 7-print_last_digit.c
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/** * times_table - Prints the 9 times table * * Return: no return */ void times_table(void) { int a, b, op; for (a = 
+0; a <= 9; a++)
+	{ _putchar(48); for (b = 0; b <= 9; b++) for (b = 1; b <= 9; b++) { op = a * b; _putchar(44);
+		}
+	}
+}
+
