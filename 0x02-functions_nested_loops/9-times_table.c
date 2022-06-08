c3bb74520cedd11b36714c491f6373fbd9658144
petosagie <petosagie@gmail.com>
2022-06-07 20:33:41 -0700

    task 9 to task 11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,21 @@
 #include "main.h"
-/** * times_table - Prints the 9 times table * * Return: no return */ void times_table(void) { int a, b, op; for (a = 
-0; a <= 9; a++)
-	{ _putchar(48); for (b = 0; b <= 9; b++) for (b = 1; b <= 9; b++) { op = a * b; _putchar(44);
+/**
+* times_table - Prints the 9 times table
+*
+* Return: no return
+*/
+void times_table(void)
+{
+	int a, b, op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putchar(44);
 		}
 	}
 }
