244f2614ea959344f4694bca3ade0b946e91eee9
petosagie <petosagie@gmail.com>
2022-06-07 20:02:07 -0700

    Task 4 to Task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,20 @@
 #include "main.h"
-/** * _abs - Computes the absolute value * of an integer. * * @i: input number as an integer. * * Return: absolute 
- value */
-int _abs(int i) { if (i >= 0) { return (i);
+/**
+* _abs - Computes the absolute value
+* of an integer.
+*
+* @i: input number as an integer.
+*
+* Return: absolute value
+*/
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
 	}
-	else { return (i * -1);
+	else
+	{
+		return (i * -1);
 	}
 }
-chmod u+x 7-print_last_digit.c
